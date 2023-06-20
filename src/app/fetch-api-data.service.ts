8a65ce7989c57e06abdaeb157166316ccd96053f
Tinnkie <Tinnkie@users.noreply.github.com>
2023-06-20 21:15:29 +0200

    create servicefor consuming REST API

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -1,9 +1,56 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
+const apiUrl = 'https://movieflix2023.herokuapp.com/movies';
 @Injectable({
   providedIn: 'root'
 })
-export class FetchApiDataService {
 
-  constructor() { }
+export class FetchApiDataService {
+// Inject the HttpClient module to the constructor params
+// This will provide HttpClient to the entire class, making it available via this.http
+ constructor(private http: HttpClient) {
 }
+
+// Making the api call for the user registration endpoint
+public userRegistration(userDetails: any): Observable<any> {
+  console.log(userDetails);
+  return this.http.post(apiUrl + 'users', userDetails).pipe(
+  catchError(this.handleError)
+  );
+}
+
+// Making the api call for the get all movies endpoint
+getAllMovies(): Observable<any> {
+  const token = localStorage.getItem('token');
+  return this.http.get(apiUrl + 'movies', {headers: new HttpHeaders(
+    {
+      Authorization: 'Bearer ' + token,
+    })}).pipe(
+    map(this.extractResponseData),
+    catchError(this.handleError)
+  );
+}
+// Non-typed response extraction
+private extractResponseData(res: any): any {
+  const body = res;
+  return body || { };
+}
+
+private handleError(error: HttpErrorResponse): any {
+  if (error.error instanceof ErrorEvent) {
+  console.error('Some error occurred:', error.error.message);
+  } else {
+  console.error(
+      `Error Status code ${error.status}, ` +
+      `Error body is: ${error.error}`);
+  }
+  return throwError(
+  'Something bad happened; please try again later.');
+}
+}
+
+
+
