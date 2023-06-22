7e8caae0693703f2d3c52e4ed9cf58e8cfe02154
Tinnkie <Tinnkie@users.noreply.github.com>
2023-06-22 18:24:30 +0200

    connect movie card components

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service'
+import { MatDialog } from '@angular/material/dialog';
+import { MovieInfoComponent } from '../movie-info/movie-info.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-movie-card',
@@ -8,7 +11,9 @@ import { FetchApiDataService } from '../fetch-api-data.service'
 })
 export class MovieCardComponent {
   movies: any[] = [];
-  constructor(public fetchApiData: FetchApiDataService) { }
+  constructor(public fetchApiData: FetchApiDataService,
+    public snackBar: MatSnackBar,
+    public dialog: MatDialog) { }
 
 ngOnInit(): void {
   this.getMovies();
@@ -21,4 +26,53 @@ getMovies(): void {
       return this.movies;
     });
   }
+
+openGenre(name: string, description: string): void {
+    this.dialog.open(MovieInfoComponent, {
+      data: {
+        title: name,
+        content: description
+      },
+    });
+  }
+
+openDirector(name: string, bio: string): void {
+    this.dialog.open(MovieInfoComponent, {
+      data: {
+        title: name,
+        content: bio
+      },
+    });
+  }
+
+openSynopsis(description: string): void {
+    this.dialog.open(MovieInfoComponent, {
+      data: {
+        title: 'Synopsis',
+        content: description
+      },
+    });
+  }
+
+addFavorite(id: string): void {
+    this.fetchApiData.addFavoriteMovie(id).subscribe((result) => {
+
+      this.snackBar.open('Movie added to favorites.', 'OK', {
+        duration: 2000
+      });
+    });
+  }
+
+isFavorite(id: string): boolean {
+    return this.fetchApiData.isFavoriteMovie(id);
+  }
+
+removeFavorite(id: string): void {
+    this.fetchApiData.deleteFavoriteMovie(id).subscribe((result) => {
+      this.snackBar.open('Movie removed from favorites.', 'OK', {
+        duration: 2000
+      });
+    });
+  }
+  
 }
